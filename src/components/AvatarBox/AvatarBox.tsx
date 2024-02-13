a8fe6c85cfddcc5be5d2a1c6e1caafc1e11338bd
Vlad Mykhatilo <mykhatilo-vlad@gmail.com>
2024-02-13 23:23:04 +0200

    adjust avatar, ready for gh-pages

--- src/components/AvatarBox/AvatarBox.tsx
+++ src/components/AvatarBox/AvatarBox.tsx
@@ -4,7 +4,7 @@ const AvatarBox = () => {
 
     return (
         <div className={styles.avatarBox}>
-            <img className={styles.avatar} src="/avatar.png" alt="avatar" width="150" height="150" />
+            <img className={styles.avatar} src="avatar.png" alt="avatar" width="150" height="150" />
             <h1>Vlad Mykhatilo</h1>
             <span>WordPress Developer</span>
         </div>
