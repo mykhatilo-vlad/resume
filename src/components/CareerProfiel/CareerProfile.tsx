e425e02b3972edc75f5963ed61a076ef33ced821
Vlad Mykhatilo <mykhatilo.vlad@gmail.com>
2024-12-04 23:43:26 +0200

    change years of experience

--- src/components/CareerProfiel/CareerProfile.tsx
+++ src/components/CareerProfiel/CareerProfile.tsx
@@ -3,7 +3,7 @@ import { UserIcon } from '../Icons';
 
 const CareerProfile = () => {
     const summary = `
-        I'm a Web Developer with over 3 years of experience in WordPress, specializing in JavaScript (JS) and PHP. 
+        I'm a Web Developer with over 4 years of experience in WordPress, specializing in JavaScript (JS) and PHP. 
         I've also worked extensively with Nuxt, Vue, and TypeScript for a year. 
         My skills include building and integrating RESTful APIs and GraphQL, using Git for version control, and working with headless CMS like Strapi. 
         Additionally, I have experience in React, Next.js, and Prisma. 
